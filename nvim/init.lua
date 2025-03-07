7ab94c93b8e46219fc58a6d57c8a84650a864842
Michael <Michael.Lindner@bush.edu>
2025-03-06 17:17:24 -0800

    global clipboard for vscode

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,7 @@
 if vim.g.vscode then
     -- VSCode extension
+    -- set clipboard to global clipboard
+    vim.opt.clipboard:append("unnamedplus")
 else
     -- ordinary Neovim
     require("mlindner")
