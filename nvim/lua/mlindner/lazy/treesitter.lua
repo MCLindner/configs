adf4a045846563efd49363df2d0e1b4e07ea5388
Michael <Michael.Lindner@bush.edu>
2025-03-11 16:09:22 -0700

    added which key and configured treesitter

--- nvim/lua/mlindner/lazy/treesitter.lua
+++ nvim/lua/mlindner/lazy/treesitter.lua
@@ -1,16 +1,31 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local configs = require("nvim-treesitter.configs")
- 
-    configs.setup({
-      ensure_installed = {
-        "c", "lua", "vim", "vimdoc" 
-      },
-      sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
+		configs.setup({
+			ensure_installed = {
+				"c",
+				"lua",
+				"vim",
+				"vimdoc",
+				"python",
+				"bash",
+			},
+			auto_install = true,
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+			ignore_install = {},
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "gnn", -- set to `false` to disable one of the mappings
+					node_incremental = "grn",
+					scope_incremental = "grc",
+					node_decremental = "grm",
+				},
+			},
+		})
+	end,
 }
